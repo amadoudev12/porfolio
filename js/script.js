cf53ac816fce330f9d257184d0d3ef3a41a085d5
amadoudev12 <delhadjamadou670@gmail.com>
2025-05-19 16:41:10 +0100

    ajout

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let responsive = document.querySelector('.responsive')
+let menu = document.querySelector('.menu')
+
+responsive.addEventListener('click',()=>{
+    menu.style.transform.translateY="0"
+})
